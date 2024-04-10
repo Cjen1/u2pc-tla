e2c7aa873bf2fe813cf8f02c317499147f122c22
cjen1 <cjj39@cam.ac.uk>
2024-04-10 13:04:54 +0100

    init

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    apalache-release = {
+      url =
+        "https://github.com/informalsystems/apalache/releases/download/v0.44.10/apalache.zip";
+      flake = false;
+      type = "tarball";
+    };
+
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        apalache = pkgs.stdenv.mkDerivation {
+          name = "apalache";
+          src = inputs.apalache-release;
+
+          buildInputs = [ pkgs.makeWrapper ];
+
+          installPhase = ''
+                mkdir -p $out
+                cp -r lib $out/lib
+
+                mkdir -p $out/bin
+                cat > $out/bin/apalache-mc <<- EOM
+                #!${pkgs.bash}/bin/bash
+                exec ${pkgs.jre}/bin/java -Xmx100G -jar "$out/lib/apalache.jar" "\$@"
+                EOM
+                chmod +x $out/bin/apalache-mc
+          '';
+
+          postFixup = ''
+            wrapProgram $out/bin/apalache-mc \
+            --set PATH ${pkgs.lib.makeBinPath [ pkgs.gcc12 pkgs.z3 ]}
+          '';
+        };
+      in {
+        devShell = pkgs.mkShell { 
+          buildInputs = [ 
+            pkgs.tlaplus
+            apalache 
+            pkgs.jre
+          ]; 
+        };
+      });
+}
